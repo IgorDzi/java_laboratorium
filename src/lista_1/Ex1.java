d01b87e830ad4a19f37bdd3b16b2b96fa19310b1
IDziubek <idziubekk@gmail.com>
2023-10-12 15:43:43 +0200

    Obsługa Błędów
    Dodano obsługę błędów w przypadku wprowadzenia złego typu danych oraz przeniesiono sprawdzania danych w miejsce ich wprowadzenia, by umożliwić ich poprawienie bez resetowani programu

--- src/lista_1/Ex1.java
+++ src/lista_1/Ex1.java
@@ -1,32 +1,57 @@
 package lista_1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
 public class Ex1 {
+
     /**
      * Metoda przyjmująca od użytkownika prędkości początkową oraz kąt uderzenia i na
      * ich podstawie wyliczą zasięg, oraz maksymalną wysokości rzutu ukośnego
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double v0;
+        while (true) {
+            System.out.print("Podaj prędkość: ");
+            try {
+                v0 = scanner.nextDouble();
+                if (v0 < 0) {throw new InputMismatchException();}
+                break;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Nieprawidłowe dane wejściowe!");
+                scanner.nextLine();
 
-        System.out.println("Podaj prędkość: ");
-        double v0 = scanner.nextDouble();
-        System.out.println("Podaj kąt uderzenia: ");
-        double angle = scanner.nextDouble();
+            }
+
+        }
+
+        double angle;
+        while (true) {
+            System.out.print("Podaj kąt uderzenia: ");
+            try {
+                angle = scanner.nextDouble();
+                if (angle < 0 || angle > 90) {throw new InputMismatchException();}
+                break;
+            }
+            catch (InputMismatchException e){
+                System.out.println("Nieprawidłowe dane wejściowe!");
+                scanner.nextLine();
+
+            }
+
+        }
+
+
         double angle_radian = Math.toRadians(angle);
-
         scanner.close();
 
-        if (v0 < 0 || angle < 0 || angle > 90) {
-            System.out.println("Nieprawidłowe dane wejściowe.");
-        } else {
             double distance = (Math.pow(v0, 2) * Math.sin(2 * angle_radian)) / 9.81;
             double maxHeight = (Math.pow(v0, 2) * Math.sin(angle_radian) * Math.sin(angle_radian) / (2 * 9.81));
 
             System.out.println("Zasięg: " + distance + " m");
             System.out.println("Maksymalna wysokość: " + maxHeight + " m");
-        }
     }
 }
