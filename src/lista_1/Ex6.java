3bee4617dffe09fd075f8441f06a283299115d62
IDziubek <idziubekk@gmail.com>
2023-10-16 12:30:56 +0200

    Obsługa wyjątków
    Poprawiono obsługę błędu złego typu danych by miała miejsce przy wprowadzaniu danych wejściowych

--- src/lista_1/Ex6.java
+++ src/lista_1/Ex6.java
@@ -1,5 +1,6 @@
 package lista_1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ex6 {
@@ -10,63 +11,69 @@ public class Ex6 {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Podaj dzień, miesiąc oraz rok: ");
-        int dzien = scanner.nextInt();
-        int miesiac = scanner.nextInt();
-        int rok = scanner.nextInt();
-
+        int dzien;
+        int miesiac;
+        int rok;
         try {
+            dzien = scanner.nextInt();
+            miesiac = scanner.nextInt();
+            rok = scanner.nextInt();
             String data2 = data(dzien, miesiac, rok);
             System.out.println(data2);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+
+        } catch (InputMismatchException e) {
+            System.out.println("Błędne wprowadzenie danych");
+            System.exit(0);
         }
     }
 
-    /**
-     * metoda sprawdzająca czy rok jest rokiem przestępnym
-     * @param year rok
-     * @return true: jeżeli jest; false: jeżeli nie jest
-     */
-    public static boolean is_leap_year(int year) {
-        boolean isLeapYear = (year % 4 == 0);
-        isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
-        return isLeapYear;
-    }
 
-    /**
-     * Metoda tworząca datę sprawdzając poprawność wprowadzenia danych
-     * @param dzien dzień
-     * @param miesiac miesiąc
-     * @param rok rok
-     * @return utworzona data
-     */
-    public static String data(int dzien, int miesiac, int rok) {
-        int dopuszczalnyDzien = 31;
-
-        if (miesiac < 1 || miesiac > 12) {
-            throw new IllegalArgumentException("Podano nieprawidlowy miesiac");
+        /**
+         * metoda sprawdzająca, czy rok jest rokiem przestępnym
+         * @param year rok
+         * @return true: jeżeli jest; false: jeżeli nie jest
+         */
+        public static boolean is_leap_year ( int year){
+            boolean isLeapYear = (year % 4 == 0);
+            isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+            return isLeapYear;
         }
 
-        if (miesiac == 4 || miesiac == 6 || miesiac == 9 || miesiac == 11) {
-            dopuszczalnyDzien = 30;
-        }
+        /**
+         * Metoda tworząca datę sprawdzając poprawność wprowadzenia danych
+         * @param dzien dzień
+         * @param miesiac miesiąc
+         * @param rok rok
+         * @return utworzona data
+         */
+        public static String data ( int dzien, int miesiac, int rok){
+            int dopuszczalnyDzien = 31;
 
-        if (miesiac == 2) {
-            if (is_leap_year(rok)){
-                dopuszczalnyDzien = 29;
-            }
-            else {
-                dopuszczalnyDzien = 28;
+            if (miesiac < 1 || miesiac > 12) {
+                throw new IllegalArgumentException("Podano nieprawidlowy miesiac");
             }
 
-        }
+            if (miesiac == 4 || miesiac == 6 || miesiac == 9 || miesiac == 11) {
+                dopuszczalnyDzien = 30;
+            }
 
-        if ((dzien < 1) || (dzien > dopuszczalnyDzien)) {
-            throw new IllegalArgumentException("Podano nieprawidłowy dzień.");
-        }
+            if (miesiac == 2) {
+                if (is_leap_year(rok)) {
+                    dopuszczalnyDzien = 29;
+                } else {
+                    dopuszczalnyDzien = 28;
+                }
 
-        String nowyDzien = String.format("%02d", dzien);
-        String nowyMiesiac = String.format("%02d", miesiac);
-        return nowyDzien + "-" + nowyMiesiac + "-" + rok;
+            }
+
+            if ((dzien < 1) || (dzien > dopuszczalnyDzien)) {
+                throw new IllegalArgumentException("Podano nieprawidłowy dzień.");
+            }
+
+            String nowyDzien = String.format("%02d", dzien);
+            String nowyMiesiac = String.format("%02d", miesiac);
+            return nowyDzien + "-" + nowyMiesiac + "-" + rok;
+        }
     }
-}
+
+
