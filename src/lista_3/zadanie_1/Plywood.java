f7abd49decff5797ad96cee17fa22a8f491f6deb
IDziubek_L <268385@student.pwr.edu.pl>
2023-10-27 12:32:55 +0200

    Lista 3
    Usunięto klasa PlywoodElement dodano metod to Plywood

--- src/lista_3/zadanie_1/Plywood.java
+++ src/lista_3/zadanie_1/Plywood.java
@@ -1,34 +1,56 @@
 package lista_3.zadanie_1;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Plywood implements Product{
     private final int quantity;
+    private final ArrayList<WoodType> woodTypes;
+    private final ArrayList<Integer> woodPercentages;
 
-    public Plywood(int quantity, PlywoodElement... elements) {
+    public Plywood(int quantity, HashMap<WoodType, Integer> elements) {
         this.quantity = quantity;
-        int check = 100;
-        for (PlywoodElement i: elements){
-            check =- i.getPercentage();
+        this.woodTypes = new ArrayList<WoodType>(elements.keySet());
+        this.woodPercentages= new ArrayList<Integer>(elements.values());
+        int check = 0;
+        try{
+            for (Integer woodPercentage : woodPercentages) {
+                check =+woodPercentage;
+            }
+            if (check != 100){
+                throw new IllegalArgumentException();
+            }
         }
-        if (check != 100){
-            throw new IllegalArgumentException();
+        catch (IllegalArgumentException e){
+            System.out.println("Percentages of wood types must add up to 100%");
         }
 
     }
 
     @Override
     public String getName() {
-        String name;
-        return null;
+        StringBuilder name = new StringBuilder(String.valueOf(woodTypes.get(0)));
+        for (int i=1; i< woodTypes.size();i++){
+            name.append(" + ").append(woodTypes.get(i));
+        }
+        return name.toString();
     }
 
     @Override
     public double getPrice() {
-        return 0;
+        double price = 0;
+        for (int i=1; i< woodPercentages.size();i++){
+            price += woodTypes.get(i).price * woodPercentages.get(i);
+        }
+        return price;
     }
 
     @Override
     public int getQuantity() {
-        return 0;
+        return quantity;
+    }
+    public String getComposition(){
+
     }
 }
