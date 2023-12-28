3a1337e936840201848f957622ae58bae746cc58
IDziubek <idziubekk@gmail.com>
2023-12-28 18:55:31 +0100

    Komentarz
    Dodano komentarz dokumentacyjny

--- src/lista_6/zadanie_1/SentenceStats.java
+++ src/lista_6/zadanie_1/SentenceStats.java
@@ -7,17 +7,26 @@ import java.util.stream.Stream;
 public class SentenceStats {
 
 
+    /**
+     * @param sentence Zdanie
+     * @return liczby słow
+     */
     protected static Optional<Integer> countWords(String sentence) {
         return Optional.of((int) Arrays.stream(sentence.split("\\s+"))
                 .filter(word -> !word.isEmpty())
                 .count());
     }
 
+    /**
+     * @param sentence Zdanie
+     * @return liczba znaków
+     */
     protected static Optional<Integer> countCharacters(String sentence) {
         return Optional.of((int) sentence.chars()
                 .filter(ch -> !Character.isWhitespace(ch))
                 .count());
     }
+
 
     protected static <T> Map<T, Long> filterAndCountOccurrences(Stream<T> elements) {
         return elements.filter(Objects::nonNull)
